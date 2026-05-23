8b58b78bc174301629902c3e9a6976f2bdcc096a
high5hatta <hatta@high5linx.com>
2026-05-23 09:36:40 +0900

    Create config.js
    firebaseの設定を別ファイル化

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCK7imPDxhC_lC9ENU767Xa-KeQ0s6sXWs",
+  authDomain: "sanrentan-3ceca.firebaseapp.com",
+  databaseURL: "https://sanrentan-3ceca-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sanrentan-3ceca",
+  storageBucket: "sanrentan-3ceca.firebasestorage.app",
+  messagingSenderId: "1013768821623",
+  appId: "1:1013768821623:web:a7f50a3c862d413abe5f72"
+};
